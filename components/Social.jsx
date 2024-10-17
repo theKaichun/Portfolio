6030f97cd800e7ec79d834fa6db1666bd73d5207
Kaichun <i1am2boss3@gmail.com>
2024-10-17 15:08:31 +0800

    add social

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,23 +1,36 @@
-import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLine, FaInstagram, FaFacebook } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaYoutube />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  {
+    icon: <FaGithub />,
+    path: "https://github.com/theKaichun",
+  },
+  { icon: <FaLine />, path: "https://line.me/ti/p/DNPP5UB6AX" },
+  { icon: <FaInstagram />, path: "https://www.instagram.com/0915_kaichun/" },
+  {
+    icon: <FaFacebook />,
+    path: "https://www.facebook.com/profile.php?id=100002735667994&locale=zh_TW",
+  },
 ];
+
 const Social = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-          <Link key={index} href={item.path} className={iconStyles}>
+          <a
+            key={index}
+            href={item.path}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={iconStyles}
+          >
             {item.icon}
-          </Link>
+          </a>
         );
       })}
     </div>
   );
 };
+
 export default Social;
