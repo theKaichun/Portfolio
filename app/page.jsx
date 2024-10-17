83121aa75a1000989cf1ff38089acec3ebabc4ab
Kaichun <i1am2boss3@gmail.com>
2024-10-17 16:00:44 +0800

    add cv

--- app/page.jsx
+++ app/page.jsx
@@ -23,14 +23,16 @@ const Home = () => {
             </p>
             {/* btn amd socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className=" uppercase flex items-center gap-2"
-              >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="/鎧君.pdf" download="鎧君.pdf">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className=" uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
